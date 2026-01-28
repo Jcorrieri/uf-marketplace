57d9d77a8df3cff242aa812595cfd95de268e303
Jacomo Corrieri <jacomocorrieri@gmail.com>
2026-01-28 18:34:06 -0500

    FINALLY set up hello world connection with backend

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -9,4 +9,17 @@ import { RouterOutlet } from '@angular/router';
 })
 export class App {
   protected readonly title = signal('UfMarketPlace');
+  apiResponse = signal('');
+
+  ngOnInit(): void {
+    fetch('/api/hello-world')
+      .then(response => response.json()) // or .json() if your API returns JSON
+      .then(data => {
+        this.apiResponse.set(data.content);
+      })
+      .catch(error => {
+        console.error('API error:', error);
+        this.apiResponse.set('error!!!!');
+      });
+  }
 }
