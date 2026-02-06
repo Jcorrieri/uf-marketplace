7a81aaa4160f4fa12a7cb1302b0e448f0c76fc8f
Shakir Gamzaev <shkir100@gmail.com>
2026-02-06 11:13:09 -0500

    feat: add robust TextField component and create login form layout

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TextField } from './components/text-field/text-field';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TextField],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('UfMarketPlace');
+  
 }
