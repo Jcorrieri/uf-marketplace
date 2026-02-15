c94bd6422c70efe11a74aa2d072860c04623c507
Pranav Padmapada <pranavpadmapada@gmail.com>
2026-02-15 17:23:51 -0500

    Add user service method to update settings

--- backend/services/user_service.go
+++ backend/services/user_service.go
@@ -27,3 +27,27 @@ func (s *UserService) GetByID(id uuid.UUID) (*models.User, error) {
 
 	return &user, nil
 }
+
+func (s *UserService) UpdateSettings(
+	id uuid.UUID,
+	username string,
+	firstName string,
+	lastName string,
+) (*models.User, error) {
+
+	var user models.User
+
+	if err := s.db.First(&user, "id = ?", id).Error; err != nil {
+		return nil, err
+	}
+
+	user.Username = username
+	user.FirstName = firstName
+	user.LastName = lastName
+
+	if err := s.db.Save(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
