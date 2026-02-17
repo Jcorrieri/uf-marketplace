a242fd506148ef293f9b8ffb8477e66e498e2356
Jacomo Corrieri <jacomocorrieri@gmail.com>
2026-02-16 19:36:54 -0500

    sort of fix for container height issues

--- frontend/src/app/views/login-page/login-page.ts
+++ frontend/src/app/views/login-page/login-page.ts
@@ -6,17 +6,27 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
-  imports: [CommonModule, ReactiveFormsModule, MatCardModule, MatFormFieldModule, MatIconModule, MatInputModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatButtonModule,
+    RouterLink
+  ],
   templateUrl: './login-page.html',
   styleUrl: './login-page.css',
 })
 export class LoginPage {
   emailControl = new FormControl('', { nonNullable: true, validators: [Validators.required, Validators.email] });
   passwordControl = new FormControl('', { nonNullable: true, validators: [Validators.required] });
-  
+
   loginForm = new FormGroup({
     email: this.emailControl,
     password: this.passwordControl,
