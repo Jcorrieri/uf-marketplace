ce992da3c9269a1b87b4602043ed24ec5019d47c
Shakir Gamzaev <shkir100@gmail.com>
2026-02-18 08:54:56 -0500

    add text fields to sign up page for username, first name and last name and create a fetch api function to call backend

--- frontend/src/app/views/sign-up-page/sign-up-page.ts
+++ frontend/src/app/views/sign-up-page/sign-up-page.ts
@@ -8,9 +8,24 @@ import { RouterLink } from '@angular/router';
   styleUrl: './sign-up-page.css',
 })
 export class SignUpPage {
+  firstName = signal('');
+  lastName = signal('');
+  username = signal('');
   email = signal('');
   password = signal('');
   showPassword = signal(false);
+
+  onFirstNameInput(event: Event) {
+    this.firstName.set((event.target as HTMLInputElement).value);
+  }
+
+  onLastNameInput(event: Event) {
+    this.lastName.set((event.target as HTMLInputElement).value);
+  }
+
+  onUsernameInput(event: Event) {
+    this.username.set((event.target as HTMLInputElement).value);
+  }
 
   onEmailInput(event: Event) {
     this.email.set((event.target as HTMLInputElement).value);
@@ -22,5 +37,35 @@ export class SignUpPage {
 
   togglePassword() {
     this.showPassword.update((v) => !v);
+  }
+
+  //Call the backend code to create the user in backend database. upon successful completion of this function, the front end takes the user to the Main Page
+  async onSignUp() {
+    const body = {
+      username: this.username(),
+      email: this.email(),
+      password: this.password(),
+      first_name: this.firstName(),
+      last_name: this.lastName(),
+    };
+
+    try {
+      const response = await fetch('http://localhost:8080/api/auth/register', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(body),
+      });
+
+      if (!response.ok) {
+        const error = await response.json();
+        console.error('Registration failed:', error);
+        return;
+      }
+
+      const data = await response.json();
+      console.log('Registration successful:', data);
+    } catch (err) {
+      console.error('Network error:', err);
+    }
   }
 }
