2f236ae3a6ebe7031f8209f440c16f4e80291dcc
Shakir Gamzaev <shkir100@gmail.com>
2026-03-02 08:59:34 -0500

    Enhances main page header and navigation
    Hides the global navigation bar when navigating to the main product listing page, providing a dedicated header experience for this view.
    Fixes a bug with misalignement of gator icon, main search textfield, and the logout button.

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, NavigationEnd, RouterOutlet } from '@angular/router';
 import { Navbar } from './components/navbar/navbar';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +9,12 @@ import { Navbar } from './components/navbar/navbar';
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
-export class App {}
+export class App {
+  showNavbar = true;
+
+  constructor(private router: Router) {
+    this.router.events.pipe(filter((e) => e instanceof NavigationEnd)).subscribe((e) => {
+      this.showNavbar = !(e as NavigationEnd).urlAfterRedirects.startsWith('/main');
+    });
+  }
+}
