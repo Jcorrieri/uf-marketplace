c4941e06dd323e65155bbcb9a71c4bdaeb8b1fd6
Jacomo Corrieri <jacomocorrieri@gmail.com>
2026-04-11 11:43:49 -0400

    Refactor frontend to be more modular and maintainable:
    - Created avatar dropdown and listing components
    - Move shared logout functionality to the auth service

--- frontend/src/app/views/main-page/main-page.ts
+++ frontend/src/app/views/main-page/main-page.ts
@@ -6,28 +6,14 @@ import { Router } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { AuthService } from '../../services/auth.service';
 import { HttpClient } from '@angular/common/http';
 import { MatButtonModule } from '@angular/material/button';
 import { MatTooltipModule } from '@angular/material/tooltip';
+
+// Our imports
+import { AuthService } from '../../services/auth.service';
 import { AvatarDropdown } from '../../avatar-dropdown/avatar-dropdown';
-
-export interface Product {
-  id: number;
-  image_count: number;
-  first_image_id: number | null;
-  title: string;
-  description: string;
-  price: number;
-  seller_name: string;
-}
-
-export interface ProductRequest {
-  key: string;
-  query: string;
-  limit: number;
-  cursor: number;
-}
+import { Listing, ListingRequest } from '../../listing/listing';
 
 @Component({
   selector: 'app-main-page',
@@ -40,17 +26,37 @@ export interface ProductRequest {
     MatButtonModule,
     MatTooltipModule,
     AvatarDropdown,
+    Listing,
   ],
   templateUrl: './main-page.html',
   styleUrl: './main-page.css',
 })
 export class MainPage implements OnInit {
+  constructor(
+    private router: Router,
+    private authService: AuthService,
+    private http: HttpClient,
+    private cdr: ChangeDetectorRef,
+  ) {}
+
+  async ngOnInit() {
+    try {
+      await this.authService.loadUser();
+    } catch {
+      // user load failed, continue anyway
+    }
+
+    const results = await this.fetchListings(this.listingRequest);
+
+    this.listings = results;
+  }
+
   searchQuery = '';
 
   // Shared by OnInit and Search
-  async fetchListings(request: ProductRequest) {
+  async fetchListings(request: ListingRequest) {
     const results = await firstValueFrom(
-      this.http.get<Product[]>('/api/listings', {
+      this.http.get<Listing[]>('/api/listings', {
         params: {
           key: request.key,
           query: request.query,
@@ -60,48 +66,36 @@ export class MainPage implements OnInit {
       })
     );
 
-    this.filteredProducts = results;
+    this.filteredListings = results;
     this.cdr.detectChanges();
     request.cursor = results[results.length - 1].id;
 
     return results;
   }
 
-  products: Product[] = [];
-  filteredProducts: Product[] = [];
-  productRequest: ProductRequest = {
+  listings: Listing[] = [];
+  filteredListings: Listing[] = [];
+  listingRequest: ListingRequest = {
     key: '',
     query: '',
     limit: 20,
     cursor: 0
   };
 
-  async ngOnInit() {
-    try {
-      await this.authService.loadUser();
-    } catch {
-      // user load failed, continue anyway
-    }
-
-    const results = await this.fetchListings(this.productRequest);
-
-    this.products = results;
-  }
-
   // search functionality
   async search() {
     const query = this.searchQuery.toLowerCase().trim();
     const key = "title"; // Hardcoded for now but leaves flexibility for later
 
-    const request = this.productRequest
+    const request = this.listingRequest
     request.key = key;
     request.query = query;
     request.cursor = 0; // Reset cursor upon new search
 
     // TODO: Maybe remove caching and just query the API every time? Not that expensive.
     if (!query) {
-      request.cursor = this.products[this.products.length - 1].id; // Use cached id for cursor here
-      this.filteredProducts = this.products;
+      request.cursor = this.listings[this.listings.length - 1].id; // Use cached id for cursor here
+      this.filteredListings = this.listings;
       return;
     }
 
@@ -112,23 +106,4 @@ export class MainPage implements OnInit {
     this.router.navigate(['/create-listing']);
   }
 
-  constructor(
-    private router: Router,
-    private authService: AuthService,
-    private http: HttpClient,
-    private cdr: ChangeDetectorRef,
-  ) {}
-
-  timeAgo(date: Date): string {
-    const now = new Date();
-    const diffMs = now.getTime() - date.getTime();
-    const diffMins = Math.floor(diffMs / 60000);
-    if (diffMins < 1) return 'Just now';
-    if (diffMins < 60) return `${diffMins}m ago`;
-    const diffHours = Math.floor(diffMins / 60);
-    if (diffHours < 24) return `${diffHours}h ago`;
-    const diffDays = Math.floor(diffHours / 24);
-    if (diffDays < 7) return `${diffDays}d ago`;
-    return date.toLocaleDateString();
-  }
 }
